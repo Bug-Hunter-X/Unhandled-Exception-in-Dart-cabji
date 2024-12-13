5c9e3e32d5606d5f08a5b2ae910db4ac48f21ae0
BugHunter-X <askzenai@gmail.com>
2024-12-13 10:22:06 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,23 @@
+```dart
+class MyClass {
+  int? _value;
+
+  MyClass(this._value);
+
+  int get value => _value ?? 0; // Return 0 if _value is null
+
+  void setValue(int newValue) {
+    if (newValue < 0) {
+      throw Exception('Value cannot be negative');
+    }
+    _value = newValue;
+  }
+}
+
+void main() {
+  final myObject = MyClass(10);
+  print('Initial value: ${myObject.value}'); // Output: 10
+
+  myObject.setValue(-5); // Throws an exception
+}
+```
